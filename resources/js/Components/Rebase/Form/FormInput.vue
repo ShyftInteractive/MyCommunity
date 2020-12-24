96964b17ade3e6bbcb63e10791ad99b0083b7898
Joe Cianflone <joe@cianflone.co>
2020-12-24 01:01:01 -0500

    ufixes issue with remove, able to delete individual pages, able to create a page

--- resources/js/Components/Rebase/Form/FormInput.vue
+++ resources/js/Components/Rebase/Form/FormInput.vue
@@ -4,7 +4,7 @@ export default {
    inheritAttrs: false,
 
    props: {
-      subify: {
+      slugify: {
          type: Boolean,
          default: false,
       },
@@ -13,13 +13,13 @@ export default {
 
    methods: {
       handleInput(e) {
-         if (this.subify) {
-            this.$emit("input", this.makesub(e.target.value))
+         if (this.slugify) {
+            this.$emit("input", this.makeSlug(e.target.value))
          } else {
             this.$emit("input", e.target.value)
          }
       },
-      makesub(v) {
+      makeSlug(v) {
          return v
             .trim()
             .toLowerCase()
